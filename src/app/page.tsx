024f0d9a524267a21add4851bd8ed2fc60317845
Genki Ando <genkiand@hawaii.edu>
2025-07-26 23:39:49 -1000

    Landing-page ver-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row, Button } from 'react-bootstrap';
+import './landing.css';
 
 /** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
+    <Container id="landing-page" className="align-middle contents justify-content-center">
+      <Row className="align-middle mt-5 pt-5 px-3 justify-content-center">
+        <Col xs={5}>
+          <div className="d-flex">
+            <Image src="uhm-logo.jpg" width="150px" alt="" />
+            <h1 className="px-2 align-left">Manoa Food Finder</h1>
+          </div>
+          <div className="justify-content-center">
+            <h4>
+              YOUR FAVORITES. ALL IN ONE PLACE
+            </h4>
+          </div>
         </Col>
 
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
+        <Col xs={6} className="mx-3 d-flex flex-column justify-content-center background">
+          <Button variant="success" className="my-1 background">New? Register here</Button>
+          <Button variant="success" className="my-1 background">Login</Button>
         </Col>
       </Row>
     </Container>
