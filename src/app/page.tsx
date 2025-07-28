24a3807348f6dbc1c4ee705d6b92230401420c1d
Joshua Crockett <jnc9513@hawaii.edu>
2025-07-27 19:48:17 -1000

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import FullWidthImage from '../components/FullWidthImage';
 import MiddleMenu from '../components/MiddleMenu';
+import Footer from '../components/Footer';
 
 const Home = () => (
   <main>
     <Container fluid id="footer">
       <MiddleMenu />
       <FullWidthImage />
+      <Footer />
     </Container>
   </main>
 );
