4936639e2e5ad668a67bf1ebf1499f3219d8a7b9
Joshua Crockett <jnc9513@hawaii.edu>
2025-07-27 19:57:50 -1000

    added a footer to map. also plan to delete the other map repository.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import FullWidthImage from '../components/FullWidthImage';
 import MiddleMenu from '../components/MiddleMenu';
+import Footer from '../components/Footer';
 
 const Home = () => (
-  <main>
-    <Container fluid id="footer">
+  <main style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+    <Container fluid style={{ flex: 1 }}>
       <MiddleMenu />
       <FullWidthImage />
     </Container>
+    <Footer />
   </main>
 );
 
